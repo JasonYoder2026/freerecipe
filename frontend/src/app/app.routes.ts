ba9039b9402c49c6d76ec7157e984d564426aeea
Jason Yoder <jason.yoder2@bsu.edu>
2026-03-25 19:04:11 -0400

    Fix url

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Landing } from './landing/landing'; 
 import { Login } from './login/login';
+import { Signup } from './signup/signup';
 
 
 export const routes: Routes = [
   { path: '', component: Landing },
-  { path: 'login', component: Login }
+  { path: 'login', component: Login },
+  { path: 'register', component: Signup}
 ];
--- frontend/src/app/landing/landing.ts
+++ frontend/src/app/landing/landing.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [RouterModule],
+  imports: [RouterLink],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
